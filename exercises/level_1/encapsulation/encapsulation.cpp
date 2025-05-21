1598be4bdaf2b59eb54c47fee63c85400f97bec4
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-21 18:16:05 +0200

    Level 1 Exercise 3 Draft

---
+++ exercises/level_1/encapsulation/encapsulation.cpp
@@ -0,0 +1,20 @@
+class BankAccount
+{
+    private :
+    int accountNumber;
+    int balance;
+
+    public :
+    int deposit()
+    {
+
+    }
+    int withdraw()
+    {
+
+    }
+    void getBalance()
+    {
+        
+    }
+};
