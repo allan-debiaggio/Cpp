7cf3173635b2fc7f91d08e9ec62168103cc0bb63
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-30 14:42:58 +0200

    Level 2 Exercise 5

--- exercises/level_2/reverse/reverse.cpp
+++ exercises/level_2/reverse/reverse.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 
+int reverse(int number);
+
 int main(void)
 {
     int number = 0;
 
     do
     {
-    std::cout << "Enter a number between 10 and 99 : ";
+    std::cout << "Enter a number beyond 10 : ";
     std::cin >> number;
     }
-    while(number < 10 || number > 99);
+    while(number < 10);
     
+    std::cout << reverse(number);
 
 
     return 0;
+}
+
+int reverse(int number)
+{
+    int reversed = 0;
+    
+    while(number > 0)
+    {
+        reversed = reversed * 10 + number % 10;
+        number /= 10;
+    }
+
+    return reversed;
 }
