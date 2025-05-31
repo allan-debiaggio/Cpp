2529b915814528c036057953b769d0d625cb6156
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-31 20:39:09 +0200

    Level 3 Exercise 1

---
+++ exercises/level_3/max_min/max_min.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int max_min(int numbers[]);
+
+int main(void)
+{
+    int numbers[] = {1,2,3,4,5,6,7,8,9}; 
+    
+    std::cout << max_min(numbers);
+    return 0;
+}
+
+int max_min(int numbers[])
+{
+    int results[] = {}; 
+
+    for(int i = 0; i < sizeof(numbers); i++)
+    {
+        if 
+    }
+}
