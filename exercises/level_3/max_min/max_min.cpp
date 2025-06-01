f573417dc61bcfce1472cbb277549618a65ac0f6
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-06-01 23:45:35 +0200

    Level 3 Exercise 1 2nd draft

--- exercises/level_3/max_min/max_min.cpp
+++ exercises/level_3/max_min/max_min.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 
-int max_min(int numbers[]);
-
 int main(void)
 {
     int numbers[] = {1,2,3,4,5,6,7,8,9}; 
     
-    std::cout << max_min(numbers);
+    // std::cout << std::minmax_elemnt(numbers);
     return 0;
-}
-
-int max_min(int numbers[])
-{
-    int results[] = {}; 
-
-    for(int i = 0; i < sizeof(numbers); i++)
-    {
-        if 
-    }
 }
